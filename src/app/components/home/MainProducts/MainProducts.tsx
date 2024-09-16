80c125105a0859c44e140fe022f2f9d4f7689cef
Wilkel Giovanni <wg@bitadata.com>
2024-09-16 11:52:23 -0400

    ADD: get products from API

--- src/app/components/home/MainProducts/MainProducts.tsx
+++ src/app/components/home/MainProducts/MainProducts.tsx
@@ -1,6 +1,20 @@
-"use client"
-export const MainProducts = () => {
-  console.log(process.env.NEXT_PUBLIC_SHOPIFY_HOST_NAME)
+import { headers } from "next/headers"
+
+const getProducts = async() => {
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOST_NAME}/admin/api/2023-10/products.json`, {
+      headers: new Headers({
+        'X-Shopify-Access-Token': process.env.SHOPIFY_AP_KEY || ""
+      })
+    }
+  )
+  const data = await response.json()
+
+  return data
+}
+export const MainProducts = async () => {
+  const products = await getProducts()
+  console.log(products)
   return (
     <section>
       <h1>MainProducts</h1>
