bb53dde5091d2b07922b72543a034c415447fec1
Wilkel Giovanni <wg@bitadata.com>
2024-10-03 07:56:16 -0400

    ADD: adding items to the shopping cart

--- src/app/components/shared/ShoppingCart/ShoppingCart.tsx
+++ src/app/components/shared/ShoppingCart/ShoppingCart.tsx
@@ -6,12 +6,30 @@ import { useShoppingCart } from "../../../../hooks/useShoppingCart"
 
 export const ShoppingCart = () => {
     const { cart } = useShoppingCart();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpen = () => setIsOpen(!isOpen);
     return (
-        <button className={styles.ShoppingCart}>
+        <button className={styles.ShoppingCart} onClick={handleOpen}>
             <span className={styles.ShoppingCart__counter}>
                 {cart.length}
             </span>
             <FaShoppingCart />
+            {isOpen && cart.length > 0 && (
+                <div className={styles.ShoppingCart__items}>
+                    {
+                        cart.map(item => (
+                            <>
+                            <p key={item?.id}>{item?.title}</p>
+                            <p>Quantity: {item?.quantity}</p>
+                            </>
+                        ))
+                    }
+                    <button className={styles.ShoppingCart__buyButton}>
+                        Buy
+                    </button>
+                </div>
+            )}
         </button>
     )
 }
