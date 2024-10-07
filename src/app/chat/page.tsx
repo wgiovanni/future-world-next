49922112f386c8f99c6ef631242b3cd95579d8e0
Wilkel Giovanni <wg@bitadata.com>
2024-10-07 08:49:35 -0400

    ADD: creating a chatbot component with the vercel SDK

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,9 @@
+import { Chat } from "../components/chat/chat";
+export default function ChatPage() {
+  return (
+    <>
+      <h1>Chatbot</h1>
+      <Chat />
+    </>
+  );
+}
---
+++ src/app/components/chat/chat.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useChat } from 'ai/react'
+
+export const Chat = () => {
+  const { messages, input, handleInputChange, handleSubmit } = useChat()
+
+  return (
+    <div>
+      {messages.map(m => (
+        <div key={m.id}>
+          {m.role}: {m.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Say something...
+          <input
+            value={input}
+            onChange={handleInputChange}
+          />
+        </label>
+      </form>
+    </div>
+  )
+}
