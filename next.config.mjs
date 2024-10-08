93248c9cc8cb5738adfa5978ed1e1a0ec424a91e
Wilkel Giovanni <wg@bitadata.com>
2024-10-07 20:39:52 -0400

    ADD: bundle analysis

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const bundleAnalyzer = withBundleAnalyzer({
+	enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig = {
   sassOptions: {
@@ -20,4 +25,4 @@ const nextConfig = {
   }
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
