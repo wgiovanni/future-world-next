97f535ad933da971fde7e71e638a9bfaf19413c2
Wilkel Giovanni <wg@bitadata.com>
2024-10-07 20:40:14 -0400

    ADD: Edge runtime

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -2,6 +2,8 @@ import Link from "next/link"
 import { getCollections } from "../../services/shopify/collections"
 import styles from './StoreLayout.module.sass'
 
+export const runtime = "edge"
+
 export default async function layout({ children }: { children: React.ReactNode }) {
   const collections = await getCollections()
   return (
